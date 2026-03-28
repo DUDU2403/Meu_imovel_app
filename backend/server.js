c6404f9812455f1d09806a3d22bd8f465f9d7719
DUDU2403 <eduardojunior3300@outlook.com>
2026-03-28 13:14:10 -0300

    fix: permitindo conexões externas no render

--- backend/server.js
+++ backend/server.js
@@ -56,4 +56,8 @@ app.put('/imoveis/:id', async (req, res) => {
     }
 });
 
-app.listen(5000, () => console.log("🚀 BACKEND EM: http://localhost:5000"));
+const PORT = process.env.PORT || 10000; 
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Servidor rodando na porta ${PORT}`);
+});
